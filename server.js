d4098ffd03e68e42b942193b58a21a83e25e3909
Jordan Montgomery <82307451+montiouchiha@users.noreply.github.com>
2022-05-06 14:35:25 +1000

    Co-authored-by: Mgor3944 <Mgor3944@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html')
-})
+    
+    //const parsedUrl = new URL(req.url, 'https://domainname.com)
+
+    //let pathName = parsedUrl.pathname
+
+    //let ext = path.extname(pathName)
+
+    //if (pathName !== '/' && pathName[pathName.length - 1] === '/') {
+        //res.writeHead(302, {'Location': pathName.slice(0, -1)})
+        //res.end()
+    //}
+
+    //if (pathName === '/') {
+        //ext = '.html'
+        //pathName = '/index.html'
+    //} else if (!ext) {
+        //ext = '.html'
+        //pathName += ext
+    //}
+    //})
+    res.sendFile(__dirname + '/dist/index.html')
+});
 
 let server = app.listen(8888, function(){
     console.log("App server is running on port 8888");
